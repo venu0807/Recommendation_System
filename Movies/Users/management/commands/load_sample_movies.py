a80c673187ecec11e98f49437f13e05f53bc139c
venu0807 <pvgreddy3@gmail.com>
2025-06-29 16:04:04 +0530

    june

--- Movies/Users/management/commands/load_sample_movies.py
+++ Movies/Users/management/commands/load_sample_movies.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import json
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.db import transaction # Import transaction for atomic operations
